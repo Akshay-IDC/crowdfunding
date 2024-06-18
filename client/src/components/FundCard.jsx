c3de09e7e833a4ab27c8957668a4c0973129d020
Akshay-IDC <76376872+Akshay-IDC@users.noreply.github.com>
2024-06-18 09:24:59 +0530

    delete the funcard when target meet or exceeds

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -6,6 +6,10 @@ import { daysLeft } from '../utils';
 const FundCard = ({ owner, title, description, target, deadline, amountCollections, image, handleClick }) => {
   const remainingDays = daysLeft(deadline);
   
+  if (amountCollections >= target) {
+    return null; // Don't render the card if the target is met or exceeded
+  }
+
   return (
     <div className="sm:w-[288px] w-full rounded-[15px] bg-[#1c1c24] cursor-pointer" onClick={handleClick}>
       <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]"/>
